a524cf85b364d47e74ba84c96b30421ce3ae7709
ka-zukii <rizky23ska@gmail.com>
2026-07-28 08:54:11 +0700

    fix: update firebase configuration to use environment variables for sensitive data

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0Ck29AAzuW6A_dt7c48Dm4Q3mEdAuUBk",
-  authDomain: "penjaga-langit-17882.firebaseapp.com",
-  projectId: "penjaga-langit-17882",
-  storageBucket: "penjaga-langit-17882.firebasestorage.app",
-  messagingSenderId: "171862228227",
-  appId: "1:171862228227:web:19f806807ffbb34b047d36",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Inisialisasi Firebase App
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const db = getFirestore(app);
